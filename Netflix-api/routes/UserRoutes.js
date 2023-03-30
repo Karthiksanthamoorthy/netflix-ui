1ba2a5e1c833345416d7f703cc76eb39d59a7d74
karthiksanthamoorthy <karthiksanthamoorthy713@gmail.com>
2023-03-30 18:25:32 +0530

    final

--- Netflix-api/routes/UserRoutes.js
+++ Netflix-api/routes/UserRoutes.js
@@ -5,7 +5,20 @@ const {
    } = require("../controllers/UserController")
 
     const router = require("express").Router();
+
+    // router.get("/", (req,res)=>{
+    //     res.send({
+    //         message: "Hello"
+    //     })
+    // })
+
+    router.get("/", (req,res)=>{
+        res.send({
+            message: "Hello from the backend"
+        });
+    });
     
+
     router.get("/liked/:email", getLikedMovies);
     router.post("/add", addToLikedMovies);
     router.put("/remove", removeFromLikedMovies);
